9ddaccb9b9f81e5a000a0a1142494237568d6ca4
EGDy <2009egdy2009@gmail.com>
2024-11-16 17:21:39 +0200

    Project

---
+++ js/42script.js
@@ -0,0 +1,13 @@
+let season = prompt("Enter season number");
+
+if (season == "1") {
+    alert("Your season is Spring.");
+} else if (season == "2") {
+    alert("Your season is Summer.");
+} else if (season == "3") {
+    alert("Your season is Autumn.");
+} else if (season == "4") {
+    alert("Your season is Winter.");
+} else {
+    alert("Unknown Season. Please enter a number in range from 1 to 4.");
+}
